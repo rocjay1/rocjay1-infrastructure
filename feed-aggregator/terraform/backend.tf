8a3c16702fa74882d9ea4b9a56030152829a8cae
Rocco Davino <jasonroc19@gmail.com>
2026-03-03 20:57:27 -0600

    chore: remove feed-aggregator terraform configuration

--- feed-aggregator/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "q9pp4eqfiwkpmklctfstate"
-    container_name       = "tfstate"
-    key                  = "feed-aggregator.tfstate"
-    use_azuread_auth     = true
-  }
-}
