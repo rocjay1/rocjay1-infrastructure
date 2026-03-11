74158517def0858673cd9c9dc897a28f0668062e
Rocco Davino <jasonroc19@gmail.com>
2026-03-11 07:29:09 -0500

    chore(infra): migrate terraform state to google cloud storage

--- cloudflare/terraform/backend.tf
+++ cloudflare/terraform/backend.tf
@@ -1,9 +1,6 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "q9pp4eqfiwkpmklctfstate"
-    container_name       = "tfstate"
-    key                  = "cloudflare-shared.tfstate"
-    use_azuread_auth     = true
+  backend "gcs" {
+    bucket = "daily-tech-brief-tfstate"
+    prefix = "cloudflare"
   }
 }
