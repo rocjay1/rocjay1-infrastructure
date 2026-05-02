8ca3c73ee95752b8c3cd9ae3f936caaffe2486f9
Rocco Davino <jasonroc19@gmail.com>
2026-05-02 10:10:43 -0500

    chore: remove uneeded remote state data block

--- feed-aggregator/terraform/main.tf
+++ feed-aggregator/terraform/main.tf
@@ -1,11 +1,3 @@
-data "terraform_remote_state" "miniflux" {
-  backend = "gcs"
-  config = {
-    bucket = "daily-tech-brief-tfstate"
-    prefix = "miniflux"
-  }
-}
-
 # Cloudflare Worker (Identity)
 resource "cloudflare_worker" "feed_aggregator" {
   account_id = var.account_id
