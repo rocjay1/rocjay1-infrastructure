82f5e0df4d1c5ceb82ff1e2327adadf9db04e2dd
Rocco Davino <jasonroc19@gmail.com>
2026-05-03 08:36:24 -0500

    fix(miniflux): enable user_project_override for billing API

--- miniflux/terraform/providers.tf
+++ miniflux/terraform/providers.tf
@@ -20,7 +20,9 @@ provider "cloudflare" {
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
+  project               = var.project_id
+  region                = var.region
+  zone                  = var.zone
+  billing_project       = var.project_id
+  user_project_override = true
 }
