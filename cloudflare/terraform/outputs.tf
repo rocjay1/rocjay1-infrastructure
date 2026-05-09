119720fb2577bc357b125cb9c8af8214e67b10b6
Rocco Davino <jasonroc19@gmail.com>
2026-05-09 15:28:25 -0500

    docs(cloudflare): remove obsolete Entra ID outputs

--- cloudflare/terraform/outputs.tf
+++ cloudflare/terraform/outputs.tf
@@ -13,28 +13,3 @@ output "zone_name" {
 output "cloudflare_team_domain" {
   value = var.cloudflare_team_domain
 }
-
-output "entra_identity_provider_id" {
-  value = cloudflare_zero_trust_access_identity_provider.entra.id
-}
-
-output "entra_group_id" {
-  value = azuread_group.cloudflare_access_users.object_id
-}
-
-output "entra_client_id" {
-  value = azuread_application.cloudflare_access.client_id
-}
-
-output "entra_client_secret" {
-  value     = azuread_application_password.cloudflare_access.value
-  sensitive = true
-}
-
-output "entra_tenant_id" {
-  value = data.azuread_client_config.current.tenant_id
-}
-
-output "cloudflare_redirect_uri" {
-  value = local.cloudflare_redirect_uri
-}
